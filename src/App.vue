5ca531588580bfb1374bd2c20020df41c284f263
Rudy Crespo <crespor@UADV-AC-306G3QP.admsu.montclair.edu>
2018-10-05 16:23:34 -0400

    successful api call

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CurrencyConverter />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CurrencyConverter from './components/CurrencyConverter.vue';
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    CurrencyConverter
   }
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/CurrencyConverter.vue
@@ -0,0 +1,36 @@
+<template>
+  
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'CurrencyConverter',
+  data () {
+    return {
+      exchange: '',
+      error: ''
+    }
+  },
+  created () {
+    this.getRecentExchange();
+  },
+  methods: {
+    getRecentExchange () {
+      const apiKey = process.env.VUE_APP_CURRENCY_API_KEY;
+      const endpoint = `http://www.apilayer.net/api/live?access_key=${apiKey}&currencies=DOP`;
+
+      axios.get(endpoint)
+        .then(response => (this.exchange = response.data))
+        .catch(error => console.log(error))
+        .then(response => console.log(response));
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
