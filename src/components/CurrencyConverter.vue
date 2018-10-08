d63d26227637949174e6d8550e5966d525dfcde4
rcrespo9 <rudy.crespo20@gmail>
2018-10-08 16:00:20 -0400

    no more v-model, methods fires on change, both inputs share same input, refactored

--- src/components/CurrencyConverter.vue
+++ src/components/CurrencyConverter.vue
@@ -1,18 +1,17 @@
 <template>
   <div>
     <label>USD
-      <input type="number" name="USD" :placeholder="USD.base" v-model.number="USD.converted" min="1">
+      <input type="number" name="USD" :placeholder="USD.base" :value="USD.converted" @input="updateExchange">
     </label>
 
     <label>USDDOP
-      <input type="number" name="USDDOP" :placeholder="USDDOP.base" v-model.number="USDDOP.converted" min="1">
+      <input type="number" name="USDDOP" :placeholder="USDDOP.base" :value="USDDOP.converted" @input="updateExchange">
     </label>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import _ from "lodash";
 
 export default {
   name: "CurrencyConverter",
@@ -34,26 +33,6 @@ export default {
   created() {
     // this.getRecentExchange();
   },
-  watch: {
-    USD: {
-      handler(newVal) {
-        const { converted } = newVal;
-        const convertedVal = this.convertUSDDOP(converted);
-
-        this.USDDOP.converted = parseFloat(convertedVal) || "";
-      },
-      deep: true
-    },
-    USDDOP: {
-      handler(newVal) {
-        const { converted } = newVal;
-        const convertedVal = this.convertUSD(converted);
-
-        this.USD.converted = parseFloat(convertedVal) || "";
-      },
-      deep: true
-    }
-  },
   methods: {
     getRecentExchange() {
       const apiKey = process.env.VUE_APP_CURRENCY_API_KEY;
@@ -69,19 +48,25 @@ export default {
         .catch(() => (this.error = true))
         .finally(() => (this.loading = false));
     },
-    convertUSD(newVal) {
+    updateExchange(e) {
       const { base } = this.USDDOP;
-      const convertedVal = newVal / base;
-      const formattedVal = this.formatCurrency(convertedVal);
+      const { name, valueAsNumber } = e.target;
+      let convertedVal;
+      let formattedVal;
 
-      return formattedVal;
-    },
-    convertUSDDOP(newVal) {
-      const { base } = this.USDDOP;
-      const convertedVal = newVal * base;
-      const formattedVal = this.formatCurrency(convertedVal);
+      if (name === "USD") {
+        convertedVal = valueAsNumber * base;
+        formattedVal = this.formatCurrency(convertedVal);
 
-      return formattedVal;
+        this.USD.converted = valueAsNumber;
+        this.USDDOP.converted = formattedVal;
+      } else if (name === "USDDOP") {
+        convertedVal = valueAsNumber / base;
+        formattedVal = this.formatCurrency(convertedVal);
+
+        this.USDDOP.converted = valueAsNumber;
+        this.USD.converted = formattedVal;
+      }
     },
     formatCurrency(val) {
       return parseFloat(val).toFixed(2);
